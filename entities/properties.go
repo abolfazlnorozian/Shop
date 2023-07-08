63bcbd07344b7eabadb702fc7091b5052998f629
abolfazlnorozian <abolfazlnorozian7@gmail.com>
2023-07-08 11:48:22 +0330

    struct/variations

---
+++ entities/properties.go
@@ -0,0 +1,12 @@
+package entities
+
+import "time"
+
+type Properties struct {
+	Id        int       `json:"_id" bson:"_id"`
+	Parent    int       `json:"parent" bson:"parent"`
+	Name      string    `json:"name" bson:"name"`
+	CreatedAt time.Time `json:"createdAt" bson:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt" bson:"updatedAt"`
+	V         int       `json:"__v" bson:"__v"`
+}
